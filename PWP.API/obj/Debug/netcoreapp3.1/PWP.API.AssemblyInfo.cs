5668e47c029f430ea62badd3b23b8d862daca55e
Cristian.Sguazzin <cristian.sguazzin.ext@errepar.com>
2024-05-12 20:58:37 -0300

    Se agrega rendermode InteractiveAuto. Se agrega using Render mode. Update net8, update mudblazor 8, update librerias .net8

--- PWP.API/obj/Debug/netcoreapp3.1/PWP.API.AssemblyInfo.cs
+++ PWP.API/obj/Debug/netcoreapp3.1/PWP.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PWP.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2991ee9d757af39f8388310315a2850133d9ce13")]
 [assembly: System.Reflection.AssemblyProductAttribute("PWP.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PWP.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
